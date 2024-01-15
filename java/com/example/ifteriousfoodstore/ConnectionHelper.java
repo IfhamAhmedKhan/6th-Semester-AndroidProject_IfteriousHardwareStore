51da3c0d815b866eb63c6c81cc49164589feb61b
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-01-15 21:39:45 +0500

    update
    updated project files and fixed the issues

--- java/com/example/ifteriousfoodstore/ConnectionHelper.java
+++
@@ -1,8 +0,0 @@
-package com.example.ifteriousfoodstore;
-
-public class ConnectionHelper {
-    public static String ip = "192.168.10.9";
-    public static String un = "Ifham Khan";
-    public static String pass = "ImAnAn007";
-    public static String db = "DBIfteriousGameStore";
-}
